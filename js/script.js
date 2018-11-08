7c8ead2449b43e8637ef8efc099e8d9d2d2c8c79
“Archana” <archana.villalba@gmail.com>
2018-11-08 17:49:46 +0100

    ball is animated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 // Welcome page, disapears on click
-
+var ball;
+var player1;
+var player2;
+var selectPlayer;
 $('.start-btn').click(function () {
   $('.starter-text').hide()
-  var selectPlayer = new SelectPlayer()
+  selectPlayer = new SelectPlayer()
   selectPlayer.draw()
-  var player1 = new Player1 ()
+  player1 = new Player1()
   player1.draw()
-  var player2 = new Player2 ()
+  player2 = new Player2()
   player2.draw()
-  var ball = new Ball ()
+  ball= new Ball()
   ball.draw()
+  $('.start-game').toggle()
 })
 
 // The game board is drawn
@@ -22,104 +26,80 @@ SelectPlayer.prototype.draw = function () {
   var img = new Image()
   img.onload = function () {
     this.ctx.drawImage(img, 350, 100, 720, 408)
-  } .bind(this); //pour forcer le context d'execution 
+  }.bind(this) // pour forcer le context d'execution
   img.src = './images/inside_frame.png'
-};
-
-// Adding Player 1
-
-function Player1(x, y, width, height) {
-  this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
 }
 
-Player1.prototype.draw = function() {
+// Display Player 1
+
+function Player1 (x, y, width, height) {
+  this.ctx = document.getElementById('canvas').getContext('2d')
+  this.x = x
+  this.y = y
+  this.width = width
+  this.height = height
+  this.x_speed = 0
+  this.y_speed = 0
+}
+
+Player1.prototype.draw = function () {
   var img = new Image()
   img.onload = function () {
     this.ctx.drawImage(img, 400, 260, 70, 86)
-  } .bind(this); //pour forcer le context d'execution 
+  }.bind(this) // pour forcer le context d'execution
   img.src = './images/mario.png'
-};
-
-// Adding Player 2
-
-function Player2(x, y, width, height) {
-  this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
 }
 
-Player2.prototype.draw = function() {
+// Display Player 2
+
+function Player2 (x, y, width, height) {
+  this.ctx = document.getElementById('canvas').getContext('2d')
+  this.x = x
+  this.y = y
+  this.width = width
+  this.height = height
+  this.x_speed = 0
+  this.y_speed = 0
+}
+
+Player2.prototype.draw = function () {
   var img = new Image()
   img.onload = function () {
     this.ctx.drawImage(img, 954, 275, 65, 63)
-  } .bind(this); //pour forcer le context d'execution 
+  }.bind(this) // pour forcer le context d'execution
   img.src = './images/toad.png'
-};
+}
 
 // Adding Ball
 
-function Ball(x, y) {
+function Ball (x, y) {
   this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = x;
-  this.y = y;
-  this.x_speed = 0;
-  this.y_speed = 0;
+  this.x = 690
+  this.y = 280
+  this.vx = 2
+  this.vy = 5
+  this.radius = 5
 }
 
-Ball.prototype.draw = function() {
+Ball.prototype.draw = function () {
   var img = new Image()
   img.onload = function () {
-    this.ctx.drawImage(img, 690, 280, 45, 43)
-  } .bind(this); //pour forcer le context d'execution 
+    this.ctx.drawImage(img, this.x, this.y, 45, 43)
+  }.bind(this)
   img.src = './images/shell.png'
-};
-
-function Player() {
-  this.paddle = new Paddle(175, 580, 500, 1000);
 }
 
+// Animating the ball
 
-function Computer() {
- this.paddle = new Paddle(175, 10, 50, 10);
+function update () {
+  ctx = document.getElementById('canvas').getContext('2d')
+  ctx.clearRect (0,0,1064,600)
+  selectPlayer.draw()
+  player1.draw()
+  player2.draw()
+  ball.draw()
+  ball.x += ball.vx
+  ball.y += ball.vy
 }
+setInterval(update, 1000)
 
-Player.prototype.render = function() {
-  this.paddle.render();
-};
-
-Computer.prototype.render = function() {
-  this.paddle.render();
-};
-
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball(200, 300);
-
-var render = function() {
-  player.render();
-  computer.render();
-  ball.render();
-};
-
-// Animating
-/*
-$('.start-game').click(function () {
-  var update = function() {
-  ball.update();
-})
-
-Ball.prototype.update = function() {
-  this.x += this.x_speed;
-  this.y += this.y_speed;
-};
-*/
