953ed859d1ad9f90a838242e9fcf04fe5732d8f6
“Archana” <archana.villalba@gmail.com>
2018-11-10 13:29:33 +0100

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # irongame-project
+
+What ?
+For this project, I wanted to create a Mario version of the classical Pong Game.
+The idea is to display 2 players that will throw a turtle shell from one side to another.
+The first one to miss the shell, loses.
+
+Why ?
+I've choosen this game in order to apply most of the notion learnt during class.
+
+How?
+I've used HTML and CSS style sheet for the design part but mostly 
+
+Main difficulties :
+The main issue, I've met was to mix all the lessons learnt independly in class.
+The calculation of the coordinates for canvas, the 2 players, the shell and the interaction between them was the most difficult part.
+
+Unsolved problems :
+I haven't been 
+
+Game instructions :
+From the welcome page, "start the game"
+Then, Player 1 can move his characters with the "up" and "down" keys.
+Player 2, with the "q" (for up) and "w" for down on the keyboard.
--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // Welcome page, disapears on click
-var ball;
-var player1;
-var player2;
-var selectPlayer;
+var ball
+var player1
+var player2
+var selectPlayer
+var shellImg = new Image()
+shellImg.src = './images/shell.png'
+var toadImg = new Image()
+  toadImg.src = './images/toad.png'
+  var marioImg = new Image()
+  marioImg.src = './images/mario.png'
+var interId 
 $('.start-btn').click(function () {
   $('.starter-text').hide()
   selectPlayer = new SelectPlayer()
@@ -11,9 +18,12 @@ $('.start-btn').click(function () {
   player1.draw()
   player2 = new Player2()
   player2.draw()
-  ball= new Ball()
+  ball = new Ball()
   ball.draw()
   $('.start-game').toggle()
+  interId = setInterval(update, 20)
+  $('.mario-lost').hide()
+  $('.toad-lost').hide()
 })
 
 // The game board is drawn
@@ -34,72 +44,193 @@ SelectPlayer.prototype.draw = function () {
 
 function Player1 (x, y, width, height) {
   this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = x
-  this.y = y
+  this.x = 0
+  this.y = 110
   this.width = width
   this.height = height
-  this.x_speed = 0
-  this.y_speed = 0
+  this.vx = 0
+  this.vy = 0
 }
 
 Player1.prototype.draw = function () {
-  var img = new Image()
-  img.onload = function () {
-    this.ctx.drawImage(img, 400, 260, 70, 86)
-  }.bind(this) // pour forcer le context d'execution
-  img.src = './images/mario.png'
+    this.ctx.drawImage(marioImg, this.x, this.y, 70, 86)
 }
 
 // Display Player 2
 
 function Player2 (x, y, width, height) {
   this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = x
-  this.y = y
+  this.x = 562
+  this.y = 120
   this.width = width
   this.height = height
-  this.x_speed = 0
-  this.y_speed = 0
+  this.vx = 0
+  this.vy = 0
 }
 
 Player2.prototype.draw = function () {
-  var img = new Image()
-  img.onload = function () {
-    this.ctx.drawImage(img, 954, 275, 65, 63)
-  }.bind(this) // pour forcer le context d'execution
-  img.src = './images/toad.png'
+    this.ctx.drawImage(toadImg, this.x, this.y, 65, 63)
 }
 
 // Adding Ball
 
 function Ball (x, y) {
   this.ctx = document.getElementById('canvas').getContext('2d')
-  this.x = 690
-  this.y = 280
-  this.vx = 2
-  this.vy = 5
-  this.radius = 5
+  this.x = 300
+  this.y = 150
+  this.vx = -2
+  this.vy = 2
 }
 
 Ball.prototype.draw = function () {
-  var img = new Image()
-  img.onload = function () {
-    this.ctx.drawImage(img, this.x, this.y, 45, 43)
-  }.bind(this)
-  img.src = './images/shell.png'
+    this.ctx.drawImage(shellImg, this.x, this.y, 45, 43)
 }
 
 // Animating the ball
 
 function update () {
   ctx = document.getElementById('canvas').getContext('2d')
-  ctx.clearRect (0,0,1064,600)
-  selectPlayer.draw()
+  ctx.clearRect(0, 0, 1064, 600)
   player1.draw()
   player2.draw()
   ball.draw()
   ball.x += ball.vx
   ball.y += ball.vy
 }
-setInterval(update, 1000)
 
+
+
+// Adding limits to the ball
+
+function update () {
+  ctx = document.getElementById('canvas').getContext('2d')
+  ctx.clearRect(0, 0, 1064, 600)
+  player1.draw()
+  player2.draw()
+  ball.draw()
+  ball.update(player1, player2)
+}
+
+Ball.prototype.update = function (player1, player2) {
+  this.x += this.vx
+  this.y += this.vy
+  var ball_topleftx = this.x
+  var ball_toplefty = this.y
+  var ball_bottomleftx = this.x
+  var ball_bottomlefty = this.y - 43
+  var ball_toprightx = this.x + 45
+  var ball_toprighty = this.y
+  var ball_bottomrightx = this.x + 45
+  var ball_bottomrighty = this.y - 43
+
+  if (this.y < 0) {
+    this.y = 10
+    this.vy = -this.vy
+  } else if (this.y > 300) {
+    this.y = 300
+    this.vy = -this.vy
+  }
+
+  if (
+    ball_topleftx < 70 &&
+    ball_toplefty <= player1.y + 86 &&
+    ball_toplefty >= player1.y - 86
+  ) {
+    this.vy = -this.vy
+    this.vx += 1.1
+  }
+
+  if (
+    ball_bottomleftx < 70 &&
+    ball_bottomlefty >= player1.y - 86 &&
+    ball_bottomlefty >= player1.y + 86
+  ) {
+    this.vy = -this.vy
+    this.vx += 1.1
+  }
+
+  if (
+    ball_toprightx > 562 &&
+    ball_toprighty <= player2.y + 63 &&
+    ball_toprighty >= player2.y - 63
+  ) {
+    //this.vy = -this.vy
+    this.vx = -this.vx
+  }
+
+  if (
+    ball_bottomrightx > 562 &&
+    ball_bottomrightx <= player2.y + 63 &&
+    ball_bottomrighty >= player2.y - 63
+  ) {
+    this.vy = -this.vy
+    this.vx = -this.vx
+  }
+
+  if (this.x < 0) {
+    $('.mario-lost').show()
+    clearInterval (interId)
+  }
+
+  if (this.x > 630) {
+    $('.toad-lost').show()
+    clearInterval (interId)
+    }
+
+}
+// Controls player 1
+
+var keysDown = {}
+
+window.addEventListener('keydown', function (event) {
+  keysDown[event.keyCode] = true
+  var value = event.keyCode
+  if (value == 81) {
+    // up arrow
+    this.player1.move(0, -8)
+  } else if (value == 87) {
+    // right arrow
+    this.player1.move(0, 8)
+  }
+})
+
+Player1.prototype.move = function (x, y) {
+  this.x += x
+  this.y += y
+  this.vx = x
+  this.vy = y
+  if (this.y < 0) {
+    // all the way to the left
+    this.y = 0
+  } else if (this.y > 250) {
+    // all the way to the right
+    this.y = 250
+  }
+}
+
+// Control player 2
+var keysUp = {}
+
+window.addEventListener('keyup', function (event) {
+  keysUp[event.keyCode] = true
+  var value = event.keyCode
+  if (value == 38) {
+    // q for up
+    this.player2.move(0, -10)
+  } else if (value == 40) {
+    // w for down
+    this.player2.move(0, 10)
+  }
+})
+
+Player2.prototype.move = function (x, y) {
+  this.x += x
+  this.y += y
+  this.vx = x
+  this.vy = y
+  if (this.y < 0) {
+    this.y = 0
+  } else if (this.y > 250) {
+    this.y = 250
+  }
+}
